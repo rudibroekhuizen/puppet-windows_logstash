f0b25ee5caf8da521fc2902a790c87f8859f2cc8
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2014-11-06 16:47:25 +0100

    Update package.pp

--- manifests/package.pp
+++ manifests/package.pp
@@ -7,7 +7,7 @@ class windows_logstash::package (
 # Download logstash package
   wget::fetch { "https://download.elasticsearch.org/logstash/logstash/logstash-1.4.2.zip":
     source      => "https://download.elasticsearch.org/logstash/logstash/logstash-1.4.2.zip",
-    destination => "c:\Windows\Temp",
+    destination => "c:/Windows/Temp",
     timeout     => 0,
     verbose     => false,
     #require     => File["/home/${username}"],
