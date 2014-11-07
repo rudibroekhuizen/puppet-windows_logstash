4208043eba43b7e0bef1fbc8812e0c03ab75b2a6
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2014-11-07 10:49:42 +0100

    Update packages.pp

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -10,6 +10,10 @@ class windows_logstash::packages (
     provider => 'chocolatey',
   }
   
+  windows_env { 'JAVA_HOME=%ProgramFiles%\Java\jre7':
+    type    => REG_EXPAND_SZ,
+  }
+  
   # Extract logstash zip file
   exec { "Extract zip file":
     command   => "7z.exe x -y \"$zipfilesFolder\\logstash-1.4.2.zip\"",
