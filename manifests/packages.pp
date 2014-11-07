db1f7342c75c5ffe978f2716ee9023e4e780f9b4
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2014-11-07 16:45:31 +0100

    Update packages.pp

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -25,12 +25,14 @@ class windows_logstash::packages {
   #}
   
   # Extract logstash zip file
-  #exec { "Extract zip file":
-  #  command   => "7z.exe x -y \"$zipfilesFolder\\logstash-1.4.2.zip\"",
-  #  path      => "C:/Program Files/7-Zip;${::path}",
-  #  cwd       => $installFolder,
-  #  creates   => "$installFolder/database/setup.exe",
-  #  logoutput => on_failure,
-  #}
+  exec { "Extract zip file":
+    command   => "7z.exe x -y \"c:\\Windows\\Temp\\logstash-1.4.2.zip\"",
+    path      => "C:/Program Files/7-Zip;${::path}",
+    cwd       => C:/Windows/Temp,
+    creates   => C:/ProgramData/logstash-1.4.2,
+    logoutput => on_failure,
+    require   => File['C:/Windows/Temp/logstash-1.4.2.zip'],
+  
+  }
   
 }
