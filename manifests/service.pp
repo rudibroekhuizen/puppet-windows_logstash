8161060ec4a740b2b23a71d32667a066397c5bfb
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-04-19 20:02:23 +0200

    Update service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,11 +2,32 @@
 #
 class windows_logstash::service {
 
+windows_service { 'test_service':
+  ensure       => present,
+  description  => 'test service description', #Optional
+  display_name => 'Test Service',             #Defaults to resource title
+  user         => 'domain\user',              #Optional
+  password     => 'supersecretpassword',      #Optional
+  binary       => 'c:\testbin.exe',
+}
+
   # Install Non-Sucking Service Manager
   package { nssm:
     ensure   => installed,
     provider => 'chocolatey',
   }
 
-nssm install Logstash C:\ProgramData\logstash-1.4.2\bin\logstash.bat "agent -f logstash-16.conf"
+  exec { 'install ls service':
+    command => 'c:\windows\nssm.exe install Logstash C:\ProgramData\logstash-1.4.2\bin\logstash.bat "agent -f logstash.conf",
+    cwd     => 'c:\windows',
+    unless  => 'c:\Windows\System32\cmd.exe /c sc query logstash',
+    require => File[ 'nssm', 'c:\logstash\logstash.bat' ],
+  }
+
+  service { 'Logstash':
+    ensure  => running,
+    require => Exec[ 'install ls service' ],
+  }
+  
+  
 
