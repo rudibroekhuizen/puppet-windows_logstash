22135c62f7bca4365acb13b1e603cfc5fdab5085
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-04-19 19:54:08 +0200

    Update service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,5 +2,11 @@
 #
 class windows_logstash::service {
 
+  # Install Non-Sucking Service Manager
+  package { nssm:
+    ensure   => installed,
+    provider => 'chocolatey',
+  }
+
 nssm install Logstash C:\ProgramData\logstash-1.4.2\bin\logstash.bat "agent -f logstash-16.conf"
 
