97035262e23eeb101886a6275c8960a178c07616
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-04-28 00:12:32 +0200

    Update service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -9,7 +9,7 @@ class windows_logstash::service {
   }
 
   exec { 'Logstash as a service':
-    command => 'c:/windows/nssm.exe install Logstash3 C:\ProgramData\logstash-1.4.2\bin\logstash.bat agent -f logstash.conf",
+    command => 'c:/windows/nssm.exe install Logstash3 C:/ProgramData/logstash-1.4.2/bin/logstash.bat agent -f logstash.conf",
     cwd     => 'c:/windows',
     unless  => 'c:/Windows/System32/cmd.exe /c sc query logstash',
     require => Package[ 'nssm' ]
