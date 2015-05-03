c191348a537aa7586e19327e226e7134c8d87b78
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-05-04 00:01:43 +0200

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,8 +2,8 @@
 #
 class windows_logstash::config {
 
-  # Cleanup .tmp files, caused by jruby bug, see https://github.com/jruby/jruby/issues/1237
-  tidy { 'resource title':
+  # Cleanup jffi*.tmp files, caused by jruby bug, see https://github.com/jruby/jruby/issues/1237
+  tidy { 'jffi_tmp':
     path => "C:/Windows/Temp/",
     recurse => 1,
     matches => [ "jffi*.*" ]
