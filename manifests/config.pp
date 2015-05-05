3ade955028ffd3205afda3cbe8b593d654b8bec9
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-05-05 22:56:36 +0200

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,6 +2,14 @@
 #
 class windows_logstash::config {
 
+  # Create the logstash config file logstash.conf using define configfile
+  file_concat { 'ls-config':
+    ensure  => 'present',
+    tag     => "LS_CONFIG_${::fqdn}",
+    path    => "C:/ProgramData/logstash/bin/logstash.conf",
+    notify  => $notify_service,
+  }
+
   # Cleanup jffi*.tmp files, caused by jruby bug, see https://github.com/jruby/jruby/issues/1237
   tidy { 'jffi_tmp':
     path => "c:/Windows/Temp/",
