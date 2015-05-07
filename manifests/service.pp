c7541726824f5a2fcfb7048f9c822b4f72724f76
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-05-07 19:34:35 +0200

    Change service path

--- manifests/service.pp
+++ manifests/service.pp
@@ -10,7 +10,7 @@ class windows_logstash::service {
 
 # Use nssm to create Logstash service  
   exec { 'Logstash as a service':
-    command => 'C:/ProgramData/chocolatey/bin/nssm.exe install Logstash C:/ProgramData/logstash-1.4.2/bin/logstash.bat agent -f logstash.conf',
+    command => 'C:/ProgramData/chocolatey/bin/nssm.exe install Logstash C:/ProgramData/logstash-1.4.2/bin/logstash.bat agent -f ../conf.d',
     unless  => "C:\\Windows\\System32\\cmd.exe /C sc query Logstash",
     require => Package[ 'nssm' ]
   }
