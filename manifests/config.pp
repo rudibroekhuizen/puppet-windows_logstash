460cbbf3be537c5579b48f13a4f6ff7b8fc85409
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-05-28 11:15:44 +0200

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -10,7 +10,7 @@ class windows_logstash::config {
 
   # Create a basic Logstash config file logstash.conf. Logstash will apply every file in the conf.d directory.
   windows_logstash::defines::configfile { 'logstash.conf': 
-    content => template("windows_logstash/logstash.conf.erb")
+    content => template("windows_logstash/logstash.conf.erb"),
     require => File["C:/ProgramData/logstash-1.4.2/conf.d"],
   }
     
