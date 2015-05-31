e316102ae613eff6c7ae97dac2674a461b4ac39a
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-05-31 12:59:06 +0200

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -9,10 +9,10 @@ class windows_logstash::config {
   }
 
   # Create a basic Logstash config file logstash.conf. Logstash will apply every file in the conf.d directory.
-  windows_logstash::defines::configfile { 'logstash.conf': 
-    content => template("windows_logstash/logstash.conf.erb"),
-    require => File["C:/ProgramData/logstash-1.4.2/conf.d"],
-  }
+  #windows_logstash::defines::configfile { 'logstash.conf': 
+  #  content => template("windows_logstash/logstash.conf.erb"),
+  #  require => File["C:/ProgramData/logstash-1.4.2/conf.d"],
+  #}
     
   # Cleanup jffi*.tmp files, caused by jruby bug, see https://github.com/jruby/jruby/issues/1237
   tidy { 'jffi_tmp':
