4ecc6a7c3c11951707e6a465f42d7a6181fc42cf
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-05-31 13:02:50 +0200

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,6 +36,7 @@
 # Copyright 2014 Your name here, unless otherwise noted.
 #
 class windows_logstash (
+  $configfile_hash = { 'example' => { content => input { generator { count => 1 } } output { file { path => "C:/Windows/Temp/logstash.output" } } }
   ){
 
   # Install package
