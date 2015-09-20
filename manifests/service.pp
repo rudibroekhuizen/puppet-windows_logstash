93580f7da6112f2c5688fd5bf5da39d0fa38599f
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-09-20 19:32:33 +0200

    Update service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -10,9 +10,7 @@ class windows_logstash::service {
 
 # Use nssm to create Logstash service  
   exec { 'Logstash as a service':
-    command => 'C:/ProgramData/chocolatey/bin/nssm.exe install Logstash C:/ProgramData/logstash-1.4.2/bin/logstash.bat &&
-                C:/ProgramData/chocolatey/bin/nssm.exe set Logstash AppParameters agent -f ../conf.d &&
-                C:/ProgramData/chocolatey/bin/nssm.exe set Logstash AppStdout C:\ProgramData\logstash-1.4.2\nssm\stdout.log',
+    command => 'C:/ProgramData/chocolatey/bin/nssm.exe install Logstash C:/ProgramData/logstash-1.4.2/bin/logstash.bat agent -f ../conf.d',
     unless  => "C:\\Windows\\System32\\cmd.exe /C sc query Logstash",
     require => Package[ 'nssm' ]
   }
