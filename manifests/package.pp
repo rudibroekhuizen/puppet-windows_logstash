2663108d03f87ff3a902652354ae6742f2dfe67f
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-09-23 14:57:12 +0200

    Update package.pp

--- manifests/package.pp
+++ manifests/package.pp
@@ -9,14 +9,15 @@ class windows_logstash::package {
   }
 
   # Set JAVA_HOME
-  registry::value { 'JAVA_HOME':
-    key   => 'HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment',
-    value => 'JAVA_HOME',
-    type  => 'expand',
-    data  => '%ProgramFiles%\Java\jre7',
+  windows::environment { 'JAVA_HOME':
+    value => 'C:\Program Files\Java\jre7'
   }
-  #windows_env { 'JAVA_HOME=%ProgramFiles%\Java\jre7':
-  #  type => REG_EXPAND_SZ,
+  
+  #registry::value { 'JAVA_HOME':
+  #  key   => 'HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment',
+  #  value => 'JAVA_HOME',
+  #  type  => 'expand',
+  #  data  => '%ProgramFiles%\Java\jre7',
   #}
   
   # Download logstash using opentable/download_file module
