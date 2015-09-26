1534d3c826743b1e1387bbc6d820708175bd03dc
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-09-26 20:35:43 +0200

    Update service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,11 +2,6 @@
 #
 class windows_logstash::service {
 
-  stage { 'post':
-  }
-
-  Stage[ 'main' ] -> Stage[ 'post' ]
-  
   # Install Non-Sucking Service Manager
   package { nssm:
     ensure   => installed,
@@ -27,11 +22,24 @@ class windows_logstash::service {
     require => Exec[ 'Logstash as a service' ]
   }
 
+  # Set nssm Logstash service sterr file
+  exec { 'Logstash stderr':
+    command => 'C:/ProgramData/chocolatey/bin/nssm.exe set Logstash AppStderr C:/ProgramData/logstash-1.4.2/nssm/stderr.log',
+    unless  => "C:\\Windows\\System32\\cmd.exe /C sc query Logstash",
+    require => Exec[ 'Logstash as a service' ]
+  }
+  
+  # Disable WM_CLOSE, WM_QUIT in the shutdown options. Without it, NSSM can't stop Logstash properly
+  exec { 'Logstash shutdown options':
+    command => 'C:/ProgramData/chocolatey/bin/nssm.exe set Logstash AppStopMethodSkip 6',
+    unless  => "C:\\Windows\\System32\\cmd.exe /C sc query Logstash",
+    require => Exec[ 'Logstash as a service' ]
+  } ->
+  
   # Start service Logstash
   service { 'Logstash':
     ensure  => running,
     #require => [Exec[ 'Logstash as a service' ], Exec[ 'Logstash stdout' ]]
-    stage    => 'post',
   }
   
 }
