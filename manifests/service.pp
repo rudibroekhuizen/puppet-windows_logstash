1be923b4124777d665e93f3ea46cdd499a31e8c5
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-09-26 08:26:41 +0200

    Reorganise exec

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,20 +2,25 @@
 #
 class windows_logstash::service {
 
-# Install Non-Sucking Service Manager
+  # Install Non-Sucking Service Manager
   package { nssm:
     ensure   => installed,
-    provider => 'chocolatey'
+    provider => 'chocolatey',
   }
 
-# Use nssm to create Logstash service  
-  exec { 'Logstash as a service':
-    command => 'C:/ProgramData/chocolatey/bin/nssm.exe install Logstash C:/ProgramData/logstash-1.4.2/bin/logstash.bat agent -f ../conf.d',
+  # Settings for all execs
+  Exec { 
+    path    => 'C:/ProgramData/chocolatey/bin'
     unless  => "C:\\Windows\\System32\\cmd.exe /C sc query Logstash",
     require => Package[ 'nssm' ]
   }
 
-# Start service Logstash
+  # Use nssm to create Logstash service  
+  exec { 'Logstash as a service':
+    command => 'nssm.exe install Logstash C:/ProgramData/logstash-1.4.2/bin/logstash.bat agent -f ../conf.d',
+  }
+
+  # Start service Logstash
   service { 'Logstash':
     ensure  => running,
     require => Exec[ 'Logstash as a service' ]
